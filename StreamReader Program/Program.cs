5f0aa5b6e971695b4e253e9b514073baa8e31361
FORHAD HOSSAIN <forhadhossaincub@gmail.com>
2022-10-14 22:29:28 +0600

    Add project files.

---
+++ StreamReader Program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamReader_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader sr = new StreamReader(@"..\..\Program.cs");
+
+            int i = 0;
+
+            for (string line = sr.ReadLine(); line != null; line = sr.ReadLine())
+            {
+                Console.WriteLine($"{++i:00} {line} ");
+            }
+
+            sr.Close();
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
